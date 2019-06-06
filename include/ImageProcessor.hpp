b6ef5988755ac72a5979151a370325dc17a66553
Igor Coelho <igor@sc0ville.com>
2019-06-06 19:15:00 +0900

    did some remodeling on the ImageProcessor to make it easier to call from another main

--- include/ImageProcessor.hpp
+++ include/ImageProcessor.hpp
@@ -10,28 +10,31 @@
 
 namespace artelab
 {
-    class ImageProcessor
-    {
-    public:
+  class ImageProcessor
+  {
+  public:
 
-        ImageProcessor(std::string mlp_file, cv::Size win_size, std::string outdir="", bool quiet=false, bool show=false);
-        virtual ~ImageProcessor();
+    ImageProcessor(std::string mlp_file, cv::Size win_size, std::string outdir="", bool quiet=false, bool show=false);
+    virtual ~ImageProcessor();
 
-        ImageProcessor& show(bool b=true);
-        ImageProcessor& set_output(std::string outdir);
+    ImageProcessor& show(bool b=true);
+    ImageProcessor& set_output(std::string outdir);
 
-        results process(std::string imagename, ArtelabDataset::barcode_image barcode);
+    results process(std::string imagename, ArtelabDataset::barcode_image barcode);
+    cv::Mat processSimple(cv::Mat img, double& angle);
+    void drawRectangles(cv::Mat& img_bb, cv::Rect rect, double angle);
 
-    private:
 
-        MLP _mlp;
-        cv::Size _winsize;
-        DirectoryInfo _output;
-        bool _show;
-        bool _quiet;
+  private:
 
-        void show_image(std::string name, cv::Mat img);
-    };
+    MLP _mlp;
+    cv::Size _winsize;
+    DirectoryInfo _output;
+    bool _show;
+    bool _quiet;
+
+    void show_image(std::string name, cv::Mat img);
+  };
 
 }
 
